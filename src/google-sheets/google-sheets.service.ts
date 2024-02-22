096b133e793d18d83e075b1a500a5e7734cac234
Gabriel Neto <gabrielsebastiao321@gmail.com>
2024-02-21 21:37:43 -0300

    refactor(google-sheet:service):Refatorei a classe do google sheet service e adicionei novas variaveis de ambiente

--- src/google-sheets/google-sheets.service.ts
+++ src/google-sheets/google-sheets.service.ts
@@ -4,10 +4,23 @@ require('dotenv').config();
 
 import { Injectable } from '@nestjs/common';
 import { google } from 'googleapis';
+import { ClassFromSheetDto } from './dto/class-from-sheet.dto';
 
 @Injectable()
 export class GoogleSheetsService {
   async getDataFromGoogleSheet() {
+    const classBruteTable = await this.getBruteTableFromGooleSheet(
+      process.env.ENEM_SHEET_NAME,
+      process.env.ENEM_SHEET_RANGE,
+    );
+
+    return this.bruteTableToClassArray(classBruteTable);
+  }
+
+  private async getBruteTableFromGooleSheet(
+    sheetName: string,
+    range: string,
+  ): Promise<any[][]> {
     const auth = new google.auth.GoogleAuth({
       keyFile: process.env.GOOGLEAPI_CREDENTIALS,
       scopes: 'https://www.googleapis.com/auth/spreadsheets',
@@ -15,56 +28,23 @@ export class GoogleSheetsService {
 
     const sheets = google.sheets({ version: 'v4', auth });
     const spreadsheetId = process.env.SPREADSHEET_ID;
-    const range = 'ENEM!A1:H515';
 
     try {
       const response = await sheets.spreadsheets.values.get({
         spreadsheetId,
-        range,
+        range: `${sheetName}!${range}`,
       });
-
-      const relevanceDictionary = {
-        NEN: 0,
-        BAI: 1,
-        MED: 2,
-        ALT: 3,
-      };
-
-      const frontDictionary = {
-        'Frente A': 1,
-        'Frente B': 2,
-        'Frente C': 3,
-      };
-
-      const [header, ...rows] = response.data.values;
-      const data = rows.map((row, index) => {
-        const [
-          great_topic_id,
-          front,
-          great_topic_name,
-          topic_name,
-          class_name,
-          relevance,
-          percentage,
-          total,
-        ] = row;
-        return {
-          id: index + 1,
-          great_topic_id,
-          front: frontDictionary[front],
-          great_topic_name,
-          topic_name,
-          class_name,
-          relevance: relevanceDictionary[relevance],
-          percentage,
-          total,
-        };
-      });
-
-      return data;
+      return response.data.values;
     } catch (err) {
       console.error('Erro ao obter dados da planilha:', err);
       throw err;
     }
+  }
+
+  private bruteTableToClassArray(table: any[][]): ClassFromSheetDto[] {
+    const [header, ...rows] = table;
+    return rows.map((row, index) => {
+      return new ClassFromSheetDto(index, row);
+    });
   }
 }
