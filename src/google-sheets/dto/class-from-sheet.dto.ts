20ac2f8f73e7181a5e243682fbb54f80a748bb2f
igorservo159 <igorservo159@gmail.com>
2024-02-23 20:14:49 -0300

    Mudando dicionários de métodos para consts

--- src/google-sheets/dto/class-from-sheet.dto.ts
+++ src/google-sheets/dto/class-from-sheet.dto.ts
@@ -10,6 +10,19 @@ export class ClassFromSheetDto {
   total: number;
 
   constructor(index: number, sheetRow: any[]) {
+    const frontDictionary = {
+      'Frente A': 1,
+      'Frente B': 2,
+      'Frente C': 3,
+    };
+
+    const relevanceDictionary = {
+      NEN: 0,
+      BAI: 1,
+      MED: 2,
+      ALT: 3,
+    };
+
     const [
       great_topic_id,
       front,
@@ -23,33 +36,12 @@ export class ClassFromSheetDto {
 
     this.id = index; // Definindo o ID diretamente
     this.great_topic_id = great_topic_id;
-    this.front = this.getFront(front); // Usando um método para converter o valor de 'front'
+    this.front = frontDictionary[front]; // Usando um método para converter o valor de 'front'
     this.great_topic_name = great_topic_name;
     this.topic_name = topic_name;
     this.class_name = class_name;
-    this.relevance = this.getRelevance(relevance); // Usando um método para converter o valor de 'relevance'
+    this.relevance = relevanceDictionary[relevance]; // Usando um método para converter o valor de 'relevance'
     this.percentage = percentage;
     this.total = total;
-  }
-
-  private getFront(front: string): number {
-    // Método para converter o valor de 'front' para um número
-    const frontDictionary: { [key: string]: number } = {
-      'Frente A': 1,
-      'Frente B': 2,
-      'Frente C': 3,
-    };
-    return frontDictionary[front];
-  }
-
-  private getRelevance(relevance: string): number {
-    // Método para converter o valor de 'relevance' para um número
-    const relevanceDictionary: { [key: string]: number } = {
-      NEN: 0,
-      BAI: 1,
-      MED: 2,
-      ALT: 3,
-    };
-    return relevanceDictionary[relevance];
   }
 }
