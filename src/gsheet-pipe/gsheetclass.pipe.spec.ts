eb0d9067d23f1d6840c293b3ac1d814501cd23ea
igorservo159 <igorservo159@gmail.com>
2024-02-23 21:35:08 -0300

    Adicionando Pipe para validar MEP dates

--- src/gsheet-pipe/gsheetclass.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { GSheetPipe } from './gsheet.pipe';
-
-describe('GSheetPipe', () => {
-  it('should be defined', () => {
-    expect(new GSheetPipe()).toBeDefined();
-  });
-});
--- src/mep/mep.controller.ts
+++ src/mep/mep.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Post, Body, HttpException } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpException,
+  UsePipes,
+} from '@nestjs/common';
 import { MepService } from './mep.service';
 import { CreateMepDto } from './dto/create-mep.dto';
-import { GSheetPipe } from 'src/gsheet-pipe/gsheet.pipe';
+import { ValidateMepDatesPipe } from 'src/pipes/ValidationMepDates.pipe';
 
 @Controller('mep')
 export class MepController {
   constructor(private readonly mepService: MepService) {}
 
   @Post()
+  @UsePipes(new ValidateMepDatesPipe())
   async create(@Body() createMepDto: CreateMepDto) {
     console.log(createMepDto);
     try {
